095de04489e47ece596607dffe2da1c7c067f7df
Emilija <emilija.rimselyte@gmail.com>
2024-12-07 22:02:37 +0200

    Bigger buttons and text and stuff

--- java.js
+++ java.js
@@ -1,11 +1,10 @@
-function togglePopup( LOL){
+function togglePopup(){
     document.getElementById("popup-1").classList.toggle("active");
 }
-function togglePopup( LOL) {
-
+function togglePopup() {
     const popup = document.getElementById('popup');
     const overlay = document.getElementById('popupOverlay');
     popup.classList.toggle('active');
     overlay.classList.toggle('active');
     popup.classList.toggle()
-  }
+}
